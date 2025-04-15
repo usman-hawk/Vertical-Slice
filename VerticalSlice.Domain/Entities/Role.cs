9fa8594c2f2f407bb15d423c9ff5f8c2e5d01ca9
usman-hawk <usman@hawklogix.com>
2025-04-15 21:07:10 +0500

    test

---
+++ VerticalSlice.Domain/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerticalSlice.Domain.Entities;
+
+public partial class Role
+{
+    public int RoleId { get; set; }
+
+    public string RoleName { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+}
